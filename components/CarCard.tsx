18eca0489e99f5595929642c553a0c48788c74bf
Ganesham <ganeshamtailor@gmail.com>
2023-06-19 12:46:00 +0530

    added cars details model and images from imagin studio car api

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,8 +4,8 @@ import { useState } from 'react'
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
-
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps{
     car:CarProps
@@ -35,7 +35,7 @@ const CarCard = ({car}: CarCardProps) => {
            </p>
 
            <div className='relative w-full h-40 my-3 object-contain'>
-            <Image src="/hero.png" alt='car model' fill priority
+            <Image src={generateCarImageUrl(car)} alt='car model' fill priority
             className='object-contain'/>
            </div>
 
@@ -73,7 +73,7 @@ const CarCard = ({car}: CarCardProps) => {
                     />
                 </div>
            </div>
-           
+           <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
         </div>
     )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,11 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
         'X-RapidAPI-Key': '8e06e5c21fmsh99733e6c95dbe8cp127360jsn6246ae71a2bb',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',{
         headers:headers,
     });
     const result = await response.json();
@@ -23,5 +25,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
   
     return rentalRatePerDay.toFixed(0);
-  };
-  
+};
+
+export const generateCarImageUrl = (car:CarProps, angle?:string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage');
+    const { make, year, model } = car;
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || "") //key
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+    return `${url}`
+}
